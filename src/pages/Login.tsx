58d9f11baa8e62cae829dd5a47ea849ccef4c0c1
Rafael Henkel <144692024+RafaelHenkel@users.noreply.github.com>
2024-06-25 22:29:08 -0300

    Add files via upload

---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import ButtonDefault from '../components/ButtonDefault';
+import PageDefault from '../components/PageDefault';
+
+function Home() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  function handleClick() {
+    alert(`Usuario logado ${username}, ${password}`);
+    setUsername('');
+    setPassword('');
+  }
+
+  return (
+    <>
+      <PageDefault>
+        <h1>Login</h1>
+        <div>
+          <form onSubmit={handleClick} style={{ display: 'flex', flexDirection: 'column', color: '#ffff' }}>
+            <label htmlFor="username">
+              Username:
+              <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label htmlFor="password">
+              Password:
+              <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <ButtonDefault label="Login" />
+          </form>
+        </div>
+      </PageDefault>
+    </>
+  );
+}
+
+export default Home;
